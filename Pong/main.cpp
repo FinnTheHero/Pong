316100abcd46dd025fb338008c07b4e352d684ac
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2023-11-04 01:54:34 +0400

    Added Raylib to the project using vcpkg and created first window

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <raylib.h>
 
 int main(void) {
-	std::cout << "Hello World!\n";
+	
+	// Window minimum dimensions & starting point
+	const int windowMinWidth = 800, windowMinHeight = 600;
+	
+	// Window Name
+	std::string windowName = "Pong";
+	
+	// Create Window
+	InitWindow(windowMinWidth,windowMinHeight,windowName.c_str());
+
+	// Make window resizable using flags
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+	SetTargetFPS(60);
+
+	// Main loop
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();
+		
+		// Set background to white
+		ClearBackground(RAYWHITE);
+		
+		// Draw FPS in top left corner
+		DrawFPS(10, 10);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 	return 0;
 }
